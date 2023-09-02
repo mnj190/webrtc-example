8b1f776bb26861c35fa52c7a14f5e69aa2cd89cc
jjm <mnj190@naver.com>
2023-09-03 01:52:26 +0900

    Initial commit

---
+++ src/test/java/com/min/webrtcexample/WebrtcExampleApplicationTests.java
@@ -0,0 +1,19 @@
+package com.min.webrtcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+
+
+
+
+
+}
